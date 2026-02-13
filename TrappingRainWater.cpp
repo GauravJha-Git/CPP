bf99b52379b5bf7ab7466265fd9e1cf69bfc356f
GauravJha-Git <gauravjha092006@gmail.com>
2026-02-14 01:50:28 +0530

    Added Own Code after learning for Traping Rain Water Problem.

--- TrappingRainWater.cpp
+++ TrappingRainWater.cpp
@@ -2,44 +2,29 @@
 #include <vector>
 using namespace std;
 
-int maxWater(vector<int> &arr) { 
-    int left = 1;
-    int right = arr.size() - 2;
+int maxWater(vector<int> &height) { 
+    int n = height.size();
+        int l = 0 ,  r = n-1;
+        int lmax = 0 , rmax=0;
+        int ans = 0;
 
-    // lMax : Maximum in subarray arr[0..left-1]
-    // rMax : Maximum in subarray arr[right+1..n-1]
-    int lMax = arr[left - 1];
-    int rMax = arr[right + 1];
+        while(l<r){
 
-    int res = 0;
-    while (left <= right) {
-      
-        // If rMax is smaller, then we can 
-        // decide the amount of water for arr[right]
-        if (rMax <= lMax) {
-          
-            // Add the water for arr[right]
-            res += max(0, rMax - arr[right]);
+            lmax = max(lmax,height[l]);
+            rmax =  max(rmax,height[r]);
 
-            // Update right max
-            rMax = max(rMax, arr[right]);
+            if(lmax<rmax){
+                ans+=(lmax-height[l]);
+                l++;
+            }
+            else{
+                ans+=(rmax-height[r]);
+                r--;
+            }
 
-            // Update right pointer as we have 
-            // decided the amount of water for this
-            right -= 1;
-        } else { 
-            // Add the water for arr[left]
-            res += max(0, lMax - arr[left]);
-
-            // Update left max
-            lMax = max(lMax, arr[left]);
-
-            // Update left pointer as we have 
-            // decided water for this
-            left += 1;
         }
-    }
-    return res;
+
+        return ans;
 }
 
 int main() {
